fc807cfacd41b7afd68346871615efeee5f28d15
SerhioTis <olegdaschuk@gmail.com>
2024-06-09 14:57:29 +0300

    pass 7th stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -30,6 +31,15 @@ func main() {
 		} else if args[0] == "echo" {
 			fmt.Fprint(os.Stdout, strings.Join(args[1:], " "), "\n")
 		} else if args[0] == "type" {
+			paths := strings.Split(os.Getenv("PATH"), ":")
+			for _, path := range paths {
+				fp := filepath.Join(path, args[1])
+				_, err = os.Stat(fp)
+				if err != nil {
+					fmt.Fprint(os.Stdout, args[1], " is ", fp)
+					break
+				}
+			}
 			if args[1] == "exit" || args[1] == "echo" || args[1] == "type" {
 				fmt.Fprint(os.Stdout, args[1], " is a shell builtin\n")
 			} else {
