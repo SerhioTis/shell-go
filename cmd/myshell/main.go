132662e0b6dcf3b015cb41f0a80f00acb7c7d038
SerhioTis <olegdaschuk@gmail.com>
2024-06-09 15:12:59 +0300

    pass 8th stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,6 +9,34 @@ import (
 	"strings"
 )
 
+func exitCmd(args []string) {
+	exitCode, err := strconv.Atoi(args[1])
+	if err != nil {
+		os.Exit(0)
+	}
+	os.Exit(exitCode)
+
+}
+
+func typeCmd(args []string) {
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	if args[1] == "exit" || args[1] == "echo" || args[1] == "type" {
+		fmt.Fprint(os.Stdout, args[1], " is a shell builtin\n")
+	} else {
+		for _, path := range paths {
+			fp := filepath.Join(path, args[1])
+			_, err := os.Stat(fp)
+			if err == nil {
+				fmt.Fprint(os.Stdout, args[1], " is ", fp, "\n")
+				return
+			}
+		}
+
+		fmt.Fprint(os.Stdout, args[1], ": not found\n")
+	}
+
+}
+
 func main() {
 	readear := bufio.NewReader(os.Stdin)
 	for {
@@ -23,28 +51,11 @@ func main() {
 		prompt = strings.TrimRight(prompt, "\n")
 		args := strings.Fields(prompt)
 		if args[0] == "exit" {
-			exitCode, err := strconv.Atoi(args[1])
-			if err != nil {
-				os.Exit(0)
-			}
-			os.Exit(exitCode)
+			exitCmd(args)
 		} else if args[0] == "echo" {
 			fmt.Fprint(os.Stdout, strings.Join(args[1:], " "), "\n")
 		} else if args[0] == "type" {
-			paths := strings.Split(os.Getenv("PATH"), ":")
-			for _, path := range paths {
-				fp := filepath.Join(path, args[1])
-				_, err = os.Stat(fp)
-				if err != nil {
-					fmt.Fprint(os.Stdout, args[1], " is ", fp)
-					break
-				}
-			}
-			if args[1] == "exit" || args[1] == "echo" || args[1] == "type" {
-				fmt.Fprint(os.Stdout, args[1], " is a shell builtin\n")
-			} else {
-				fmt.Fprint(os.Stdout, args[1], ": not found\n")
-			}
+			typeCmd(args)
 		} else {
 			fmt.Fprint(os.Stdout, prompt, ": command not found\n")
 		}
