3beb4c835651b67f8331cb679d86d191c8c5a51a
SerhioTis <olegdaschuk@gmail.com>
2024-06-09 14:13:54 +0300

    pass 4st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,6 +18,16 @@ func main() {
 		if err != nil {
 			fmt.Fprint(os.Stdout, err)
 		}
-		fmt.Fprint(os.Stdout, strings.TrimRight(prompt, "\n"), ": command not found\n")
+
+		prompt = strings.TrimRight(prompt, "\n")
+		args := strings.Fields(prompt)
+		if args[0] == "exit" {
+			exitCode, err := strconv.Atoi(args[1])
+			if err != nil {
+				os.Exit(0)
+			}
+			os.Exit(exitCode)
+		}
+		fmt.Fprint(os.Stdout, prompt, ": command not found\n")
 	}
 }
