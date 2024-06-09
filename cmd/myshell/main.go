085b97f4c0bed9d3c3d1cd60743ac8df831aa08f
SerhioTis <olegdaschuk@gmail.com>
2024-06-09 13:52:04 +0300

    pass 1st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	prompt, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprint(os.Stdout, err)
+	}
+	fmt.Fprint(os.Stdout, strings.TrimRight(prompt, "\n"), ": command not found\n")
 }
