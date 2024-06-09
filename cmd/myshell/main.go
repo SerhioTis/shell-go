e2dcabb928350978c45cc9519f38582961620abe
SerhioTis <olegdaschuk@gmail.com>
2024-06-09 14:26:10 +0300

    pass 6th stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -29,6 +29,12 @@ func main() {
 			os.Exit(exitCode)
 		} else if args[0] == "echo" {
 			fmt.Fprint(os.Stdout, strings.Join(args[1:], " "), "\n")
+		} else if args[0] == "type" {
+			if args[1] == "exit" || args[1] == "echo" || args[1] == "type" {
+				fmt.Fprint(os.Stdout, args[1], " is a shell builtin\n")
+			} else {
+				fmt.Fprint(os.Stdout, args[1], ": not found\n")
+			}
 		} else {
 			fmt.Fprint(os.Stdout, prompt, ": command not found\n")
 		}
